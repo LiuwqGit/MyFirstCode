54a1dfa86a4832b5e49f2a843b19edaa7926485a
LiuwqGit <772863869@qq.com>
2014-03-26 15:13:38 +0800

    数字的出现规则
    1
    2 3
    4 5 6 7
    8 9 10 11 12 。。。

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            int[] array = new int[150];
+            int i = 1;
+            for (int j = 1; j < array.Length; j++)
+            {
+                if (j == 2 * i)
+                {
+                    Console.WriteLine(" ");
+                    Console.Write(j + " ");
+                    i = j;
+                }
+                else
+                {
+                    Console.Write(j + " ");
+                }
+            }
+            Console.WriteLine("pause");
+        }
+    }
+}
